eae12ffbb8da1262522e6fe42ff740ebc7ee9755
unknown <mathias.kok@gmail.com>
2017-12-09 17:06:47 +0100

    Initial commit. Added Currying/Uncurrying. Added Option.

---
+++ FSharpUtilities/General.fs
@@ -0,0 +1,12 @@
+﻿namespace FSharpUtilities
+module General =
+  
+  let uncurry2 f (x, y) = f x y
+  let uncurry3 f (x, y, z) = f x y z
+  let uncurry4 f (x, y, z, t) = f x y z t
+  let uncurry5 f (x, y, z, t, u) = f x y z t u
+
+  let curry2 f x y = f (x, y)
+  let curry3 f x y z = f (x, y, z)
+  let curry4 f x y z t = f (x, y, z, t)
+  let curry5 f x y z t u = f (x, y, z, t, u)
---
+++ FSharpUtilities/Option.fs
@@ -0,0 +1,26 @@
+﻿namespace FSharpUtilities
+
+open System
+
+module Option =
+  
+  let (>>=) = Option.bind
+
+  let apply mf mx = 
+    match mf with
+    | Some f -> Option.map f mx
+    | None -> None
+
+  let (<*>) = apply
+
+  let mapObj mapper x =
+    let res = mapper x
+    match res with
+    | null -> None
+    | _ -> Some res
+
+  let mapNullable (mapper : 'a -> 'b Nullable) x =
+    let res = mapper x
+    match res.HasValue with
+    | true -> Some res
+    | false -> None
