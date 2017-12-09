78720cb267a06bad7c85918a17117b3904bad0fe
unknown <mathias.kok@gmail.com>
2017-12-09 17:47:50 +0100

    Added ResultBuilder.

---
+++ FSharpUtilities/Result/ResultBuilder.fs
@@ -0,0 +1,13 @@
+﻿namespace FSharpUtilities
+
+open FSharpUtilities.Result
+
+module ResultBuilder =
+
+  type ResultBuilder() = 
+    member __.Bind(r, binder) = bind r binder
+
+    member __.Return(s) = Success s
+
+
+  let result = ResultBuilder()
