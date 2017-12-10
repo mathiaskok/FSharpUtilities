1dc38c1103a08e953161f3a17081f210bc1b4a2a
unknown <mathias.kok@gmail.com>
2017-12-10 20:03:39 +0100

    Changed Option maps and binds.

--- FSharpUtilities/Option.fs
+++ FSharpUtilities/Option.fs
@@ -3,8 +3,24 @@
 open System
 
 module Option =
-  
-  let (>>=) = Option.bind
+
+  let mapObj mapper mx = 
+    match mx with
+    | None -> None
+    | Some x -> 
+      let res = mapper x
+      match res with
+      | null -> None
+      | _ -> Some res
+
+  let mapNullable (mapper: 'a -> 'b Nullable) mx =
+    match mx with
+    | None -> None
+    | Some x ->
+      let res = mapper x
+      match res.HasValue with
+      | true -> Some res.Value
+      | false -> None
 
   let apply mf mx = 
     match mf with
@@ -13,14 +29,26 @@ module Option =
 
   let (<*>) = apply
 
-  let mapObj mapper x =
-    let res = mapper x
-    match res with
-    | null -> None
-    | _ -> Some res
+  let (>>=) = Option.bind
+  
+  let bindObj mx binder =
+    match mx with
+    | None -> None
+    | Some x ->
+      let res = binder x
+      match res with
+      | null -> None
+      | _ -> Some res
 
-  let mapNullable (mapper : 'a -> 'b Nullable) x =
-    let res = mapper x
-    match res.HasValue with
-    | true -> Some res
-    | false -> None
+  let (>>=!) = bindObj
+
+  let bindNullable mx (binder: 'a -> 'b Nullable) =
+    match mx with
+    | None -> None
+    | Some x -> 
+      let res = binder x
+      match res.HasValue with
+      | true -> Some res.Value
+      | false -> None
+
+  let (>>=?) = bindNullable
