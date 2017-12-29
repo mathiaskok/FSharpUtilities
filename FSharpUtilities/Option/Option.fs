496de630616e5ac7a2e60858337eed9b3662f1f9
mathiaskok <mathias.kok@gmail.com>
2017-12-29 18:28:35 +0100

    Added OptionBuilder (maybe)

---
+++ FSharpUtilities/Option/Option.fs
@@ -0,0 +1,31 @@
+﻿module FSharpUtilities.Option
+
+open FSharpUtilities.General
+open System
+
+let mapObj mapper mx = 
+  match mx with
+  | None -> None
+  | Some x -> mapper x |> Option.ofObj
+
+let mapNullable (mapper: 'a -> 'b Nullable) mx =
+  match mx with
+  | None -> None
+  | Some x -> mapper x |> Option.ofNullable
+
+let apply mf mx = 
+  match mf with
+  | Some f -> Option.map f mx
+  | None -> None
+
+let (<*>) = apply
+
+let (>>=) o f = flip Option.bind o f
+
+let bindObj x y = flip mapObj x y
+
+let (>>=!) = bindObj
+
+let bindNullable x y = flip mapNullable x y
+
+let (>>=?) = bindNullable
---
+++ FSharpUtilities/Option/OptionBuilder.fs
@@ -0,0 +1,8 @@
+﻿module FSharpUtilities.OptionBuilder
+
+type OptionBuilder() =
+  member __.Return(x) = Some x
+  member __.ReturnWith(o) = o
+  member __.Bind(o, b) = Option.bind b o
+
+let maybe = OptionBuilder()
