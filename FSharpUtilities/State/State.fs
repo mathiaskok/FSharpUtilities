4ce55ee45730e49efcfb999e8eee9a9a6b421cbd
mathiaskok <mathias.kok@gmail.com>
2018-01-04 19:57:45 +0100

    Moved general State monad stuff into separate file. Added state functor implementation.

---
+++ FSharpUtilities/State/State.fs
@@ -0,0 +1,13 @@
+module FSharpUtilities.State
+
+type State<'s, 'a> = 's -> 'a * 's
+
+let map mapper (state: State<'s, 'a>) : State<'s, 'b> =
+  fun s ->
+    let (a, state) = state s
+    (mapper a, state)
+
+let bind (state: State<'s, 'a>) (binder: 'a -> State<'s, 'b>): State<'s, 'b> = 
+  fun s ->
+    let (a, state) = state s
+    binder a state
---
+++ FSharpUtilities/State/StateBuilder.fs
@@ -0,0 +1,12 @@
+﻿module FSharpUtilities.StateBuilder
+
+open FSharpUtilities.State
+
+type StateBuilder() =
+  member __.Return(x) : State<'s, 'a> = fun s -> (x, s)
+  
+  member __.ReturnFrom(s) = s
+
+  member __.Bind(state, binder) = bind state binder
+
+let state = StateBuilder()
