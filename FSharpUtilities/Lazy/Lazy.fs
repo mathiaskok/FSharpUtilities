cf42ab0d95de88d736458c3133f19fa41cf34780
mathiaskok <mathias.kok@gmail.com>
2018-01-04 22:05:09 +0100

    Added Lazy join

--- FSharpUtilities/Lazy/Lazy.fs
+++ FSharpUtilities/Lazy/Lazy.fs
@@ -1,19 +1,22 @@
-﻿namespace FSharpUtilities
-module Lazy =
+﻿module FSharpUtilities.Lazy
 
-  let map mapper (lx: 'a Lazy) = 
-    lazy (lx.Value |> mapper)
+let map mapper (lx: 'a Lazy) = 
+  lazy (lx.Value |> mapper)
 
-  let apply (lf: ('a -> 'b) Lazy) (lx: 'a Lazy) =
-    lazy (lf.Value lx.Value)
+let apply (lf: ('a -> 'b) Lazy) (lx: 'a Lazy) =
+  lazy (lf.Value lx.Value)
 
-  let (<*>) = apply
+let (<*>) = apply
 
-  let bind (lx: 'a Lazy) (binder: 'a -> 'b Lazy) =
-    lazy ((binder lx.Value).Value)
+let bind (lx: 'a Lazy) (binder: 'a -> 'b Lazy) =
+  lazy ((binder lx.Value).Value)
 
-  let map2 mapper (lx: 'a Lazy) (ly: 'b Lazy) =
-    map mapper lx <*> ly
+let (>>=) = bind
 
-  let map3 mapper (lx: 'a Lazy) (ly: 'b Lazy) (lz: 'c Lazy) =
-    map mapper lx <*> ly <*> lz
+let join ll = bind ll id
+
+let map2 mapper (lx: 'a Lazy) (ly: 'b Lazy) =
+  map mapper lx <*> ly
+
+let map3 mapper (lx: 'a Lazy) (ly: 'b Lazy) (lz: 'c Lazy) =
+  map mapper lx <*> ly <*> lz
