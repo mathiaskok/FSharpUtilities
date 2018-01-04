9f7cbc612e2e1690c122fa07d9d40e68900735bc
mathiaskok <mathias.kok@gmail.com>
2018-01-05 00:05:16 +0100

    More IReadOnlyDictionary stuff.

--- FSharpUtilities/Dictionary/ReadOnlyDictionary.fs
+++ FSharpUtilities/Dictionary/ReadOnlyDictionary.fs
@@ -1,31 +1,39 @@
 module FSharpUtilities.Dictionary.ReadOnly
 
 open System.Collections.Generic
+open System.Xml.Linq
 
-let b = Map.forall
-
-let c = Seq.fold
-
-let private toKvpPredicate p (kvp: KeyValuePair<'k, 'v>) = p kvp.Key kvp.Value
+let private toKvpFun p (kvp: KeyValuePair<'k, 'v>) = p kvp.Key kvp.Value
 
 let private toKvpFolder f =
   fun (s: 's) (kvp: KeyValuePair<'k, 'v>) -> f s kvp.Key kvp.Value
+
+let private kvpToTuple (kvp: KeyValuePair<'a, 'b>) = (kvp.Key, kvp.Value)
 
 let count (dict: IReadOnlyDictionary<'a, 'b>) = dict.Count
 
 let containsKey (dict: IReadOnlyDictionary<'a, 'b>) = dict.ContainsKey
 
 let exits predicate (dict: IReadOnlyDictionary<'a, 'b>) = 
-  Seq.exists (toKvpPredicate predicate) dict
-
-//filter
+  Seq.exists (toKvpFun predicate) dict
 
 let find a (dict: IReadOnlyDictionary<'a, 'b>) = dict.[a]
 
-let findKey predicate (dict: IReadOnlyDictionary<'a, 'b>) =
-  match Seq.tryFind (toKvpPredicate predicate) dict with
-  | Some k -> k.Key
-  | None -> raise (KeyNotFoundException())
+let tryFind a (dict: IReadOnlyDictionary<'a, 'b>) =
+  match dict.TryGetValue a with
+  | (true, x) -> Some x
+  | (false, _) -> None
+
+let findKeyI cont mapper predicate (dict: IReadOnlyDictionary<'k, 'v>) =
+  match Seq.tryFind (toKvpFun predicate) dict with
+  | Some k -> mapper k.Key
+  | None -> cont()
+
+let findKey predicate dict =
+  findKeyI (raise (KeyNotFoundException())) id predicate dict 
+
+let tryFindKey predicate dict =
+  findKeyI (fun _ -> None) Some predicate dict
 
 let fold (folder: 'a -> 'b -> 'c -> 'a) state (dict: IReadOnlyDictionary<'b, 'c>) =
   Seq.fold (toKvpFolder folder) state dict
@@ -33,9 +41,54 @@ let fold (folder: 'a -> 'b -> 'c -> 'a) state (dict: IReadOnlyDictionary<'b, 'c>
 //foldback
 
 let forall predicate (dict: IReadOnlyDictionary<'a, 'b>) =
-  Seq.forall (toKvpPredicate predicate) dict
+  Seq.forall (toKvpFun predicate) dict
 
-let tryFind a (dict: IReadOnlyDictionary<'a, 'b>) =
-  match dict.TryGetValue a with
-  | (true, x) -> Some x
-  | (false, _) -> None
+let isEmpty: IReadOnlyDictionary<'a, 'b> -> bool = Seq.isEmpty
+
+let iter action (dict: IReadOnlyDictionary<'a, 'b>) =
+  Seq.iter (toKvpFun action) dict
+
+//map
+
+//filter
+
+//ofArray
+
+//ofList
+
+//ofSeq
+
+let toSeq (dict: IReadOnlyDictionary<'a, 'b>) =
+  Seq.map kvpToTuple dict
+
+let toList (dict: IReadOnlyDictionary<'a, 'b>) =
+  List.ofSeq (toSeq dict)
+
+let toArray (dict: IReadOnlyDictionary<'a, 'b>) =
+  let arr: ('a * 'b) array = Array.zeroCreate dict.Count
+  let mutable i = 0
+  let action kvp = 
+    arr.[i] <- kvpToTuple kvp
+    i <- i+1
+
+  Seq.iter action dict
+  arr    
+
+//partition
+
+let private pickI cont mapper chooser (dict: IReadOnlyDictionary<'k, 'v>) =
+  let valid = 
+    dict
+    |> Seq.map (toKvpFun chooser)
+    |> Seq.filter Option.isSome
+
+  match Seq.tryHead valid with
+  | Some (Some x) -> mapper x
+  | _ -> cont()
+
+
+let pick chooser dict =
+  pickI (raise (KeyNotFoundException())) id chooser dict
+
+let tryPick chooser dict = 
+  pickI (fun _ -> None Some chooser dict
