c8214eaa8a0804c930b8e20408244abc18074100
Mathias Kok <mathias.kok@gmail.com>
2019-06-16 13:43:16 +0200

    Refactored Writer

--- FSharpUtilities/Writer.fs
+++ FSharpUtilities/Writer.fs
@@ -1,18 +1,15 @@
-﻿namespace FSharpUtilities
-module Writer =
-  
-  type WriterBuilder<'a>
-    (
-      zero: 'a,
-      monoid: 'a -> 'a -> 'a
-    ) =
+﻿module FSharpUtilities.Writer  
 
-    member __.Return(a) = (a, zero)
+type WriterBuilder<'a>
+  (
+    zero: 'a,
+    monoid: 'a -> 'a -> 'a
+  ) =
+  inherit StandardBuilderFunctions.BuilderBase()
 
-    member __.ReturnFrom(w) = w
+  member __.Return(a) = (a, zero)
+  member __.Bind((a, acc), writer) =
+    let (a', acc') = writer a
+    (a', monoid acc acc')
 
-    member __.Bind((a, acc), writer) =
-      let (a', acc') = writer a
-      (a', monoid acc acc')
-
-  let writer zero monoid = WriterBuilder(zero, monoid)
+let writer zero monoid = WriterBuilder(zero, monoid)
